bc5f7c63e3e900f999c69af7e2fa7a9b50f932d4
Shakir <muhammad.shakir@synergy-it.com>
2023-03-26 00:11:22 +0500

    bar chats

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,6 +1,5 @@
-import { Component, ViewChild } from '@angular/core';
-import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
-import { BaseChartDirective } from 'ng2-charts';
+import { Component, OnInit } from '@angular/core';
+import { ChartOptions, ChartType, ChartDataset } from 'chart.js';
 
 // import DataLabelsPlugin from 'chartjs-plugin-datalabels';
 
@@ -10,70 +9,30 @@ import { BaseChartDirective } from 'ng2-charts';
   styleUrls: ['./welcome.component.css'],
 })
 export class WelcomeComponent {
-  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
-
-  public barChartOptions: ChartConfiguration['options'] = {
+  public barChartOptions: ChartOptions = {
     responsive: true,
-    // We use these empty structures as placeholders for dynamic theming.
-    scales: {
-      x: {},
-      y: {
-        min: 10,
-      },
-    },
-    plugins: {
-      legend: {
-        display: true,
-      },
-      // datalabels: {
-      //   anchor: 'end',
-      //   align: 'end'
-      // }
-    },
   };
+  public barChartLabels: any = [
+    '2006',
+    '2007',
+    '2008',
+    '2009',
+    '2010',
+    '2011',
+    '2012',
+  ];
   public barChartType: ChartType = 'bar';
+  public barChartLegend = true;
+  public barChartPlugins = [];
 
-  public barChartData: ChartData<'bar'> = {
-    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
-    datasets: [
-      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
-    ],
-  };
+  public barChartData: any = [
+    { data: [50], label: 'Series A', stack: 'a' },
+    { data: [50], label: 'Series C', stack: 'a' },
+    { data: [50], label: 'Series D', stack: 'a' },
+    { data: [60], label: 'Series E', stack: 'a' },
+  ];
 
-  // events
-  // public chartClicked({
-  //   event,
-  //   active,
-  // }: {
-  //   event?: ChartEvent;
-  //   active?: {}[];
-  // }): void {
-  //   console.log(event, active);
-  // }
+  constructor() {}
 
-  // public chartHovered({
-  //   event,
-  //   active,
-  // }: {
-  //   event?: ChartEvent;
-  //   active?: {}[];
-  // }): void {
-  //   console.log(event, active);
-  // }
-
-  public randomize(): void {
-    // Only Change 3 values
-    this.barChartData.datasets[0].data = [
-      Math.round(Math.random() * 100),
-      59,
-      80,
-      Math.round(Math.random() * 100),
-      56,
-      Math.round(Math.random() * 100),
-      40,
-    ];
-
-    this.chart?.update();
-  }
+  ngOnInit() {}
 }
